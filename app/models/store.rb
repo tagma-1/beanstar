352fed7122f00856fcc9894612660503a86e32e8
tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-11-03 10:39:38 +0000

    Complete store form

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,7 +5,6 @@ class Store < ApplicationRecord
   validates :name, presence: true
   validates :about, presence: true, length: {maximum: 400}
   validates :roasting_schedule, presence: true
-  validates :pickup, presence: true
   validates :state, presence: true
 
   with_options if: :pickup? do |location|
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   root "pages#home"
   
   #Devise controllers have been generated to allow for custom re-direction to create a profile on sign up. 
-  Rails.application.routes.draw do
-  resources :stores
-    devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
-    }
-  end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   
   resources :profiles
 end
