b9e20ccf3c6043261a433b692c082561ce7f4556
tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-11-06 00:13:41 +0000

    Stlying of conversation inbox and chat display

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,6 @@
 module MessagesHelper
   
-  #A helper to determine the name of the person who is not the current user in a conversation
+  #A helper to set the profile name of the person who is not the current user in a conversation
   def other_party
     @conversation.recipient_id == current_user.id ? User.find(@conversation.sender_id).profile.profile_name.capitalize : User.find(@conversation.recipient_id).profile.profile_name.capitalize
   end
