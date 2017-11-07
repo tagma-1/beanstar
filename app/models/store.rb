d1edf3327967531cd67c61dc9b43c67e072c6e18
tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-11-07 03:56:19 +0000

    Add postcode API functionality

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,16 +3,17 @@ class Store < ApplicationRecord
   belongs_to :user
   has_many :listings, dependent: :destroy
   
+
   validates :name, presence: true
   validates :about, presence: true, length: {maximum: 400}
   validates :roasting_schedule, presence: true
   validates :state, presence: true
-
+  validates :postcode, presence: true, numericality: { less_than_or_equal_to: 9999 }
+  
   with_options if: :pickup? do |location|
     location.validates :address, presence: true
     location.validates :suburb, presence: true
-    location.validates :postcode, presence: true
   end  
-   
-
+  
+  
 end
